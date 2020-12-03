a5facd295e1e1d673c904433b0138ba7e069181d
Sofia G <55286876+Sgeis18@users.noreply.github.com>
2020-12-02 22:05:53 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Sofia Geislinger', // Title for your website.
     tagline: 'Moving through the world with a subtley sarcastic perspective.',
-    profilePhoto: 'img/profile.png',
+    profilePhoto: 'assets/profilepicCrop.jpg',
     url: 'https://Sgeis18.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
